21a693ce88307024d53ad6cb4430e70c3a828450
Bart <82309145+Duke-Skygawker@users.noreply.github.com>
2022-03-01 18:00:32 +0100

    bare bones

---
+++ main/app.js
@@ -0,0 +1,11 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", (e) => {
+    let userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+  })
+);
